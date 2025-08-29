76d93cfdcba491e21586f8d0b122151cc129d938
Anton Kolmakov <akolmakov-v@yandex.ru>
2025-08-29 17:55:12 +0300

    Выполнено задание, написал сервис с методом /reports, а также Docker файл, метод защищен PKCE, внесены правки в клиент, realm файл с описанием конфигурации KeyCloak + поправил DockerCompose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
-import Keycloak, { KeycloakConfig } from 'keycloak-js';
+import Keycloak, { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js';
 import ReportPage from './components/ReportPage';
 
 const keycloakConfig: KeycloakConfig = {
   url: process.env.REACT_APP_KEYCLOAK_URL,
   realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||"",
 };
 
 const keycloak = new Keycloak(keycloakConfig);
 
+const initOptions: KeycloakInitOptions = {
+  onLoad: 'login-required', // Автоматически перенаправлять на логин
+  pkceMethod: 'S256',       // Включить PKCE
+  checkLoginIframe: false,
+  enableLogging: true,      // Для отладки
+  flow: 'standard'          // Authorization Code Flow with PKCE
+};
+
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
       <div className="App">
         <ReportPage />
       </div>
